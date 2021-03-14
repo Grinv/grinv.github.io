5b8c722b0b91b3eb424b66b8cbe0b0faab39681f
Konstantin Ostrovskiy <k.ostrovskiy2@atol.ru>
2021-03-14 12:51:42 +0300

    delete build

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faInstagram, faFacebookF, faVk} from '@fortawesome/free-brands-svg-icons'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Школа по сёрфскейту в Москве
+        <div class="links">
+          <a href="https://instagram.com/californian.moscow">
+            <FontAwesomeIcon icon={faInstagram} size="lg" />
+          </a>
+          <a href="https://facebook.com/californian.moscow">
+            <FontAwesomeIcon icon={faFacebookF} size="lg" />
+          </a>
+          <a href="https://vk.com/californian.moscow">
+            <FontAwesomeIcon icon={faVk} size="lg" />
+          </a>
+        </div>
       </header>
     </div>
   );
